73e753d5d8e291f152fbd06f08a568f515ec6ea7
dhanasp <dhanashrinp3009@gmail.com>
2018-03-19 19:53:34 +0530

    Added functionality for checking balance

---
+++ src/test/java/com/thoughtworks/Account.java
@@ -0,0 +1,16 @@
+package com.thoughtworks;
+
+public class Account {
+    private final String accountNumber;
+    private final int balance;
+
+    public Account(String accountNumber, int balance) {
+
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
