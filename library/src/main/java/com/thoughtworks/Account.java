47d87665786fdfd26eee3dc1d822f754d85f933a
dhanasp <dhanashrinp3009@gmail.com>
2018-03-20 14:53:41 +0530

    added validation for account number with some specific condition

--- library/src/main/java/com/thoughtworks/Account.java
+++ library/src/main/java/com/thoughtworks/Account.java
@@ -1,15 +1,24 @@
 package com.thoughtworks;
+import java.util.regex.*;
 
 public class Account {
     private final String accountNumber;
     private final float balance;
 
-    public Account(String accountNumber, float balance) throws MinimumExceptionError {
+    public Account(String accountNumber, float balance) throws MinimumExceptionError, InvalidAccountNumberException {
+        validateAccountNumber(accountNumber);
         this.accountNumber = accountNumber;
         if (balance<=200){
             throw new MinimumExceptionError();
         }
         this.balance = balance;
+    }
+
+    private static void validateAccountNumber(String accountNumber) throws InvalidAccountNumberException {
+        boolean match=Pattern.matches("\\d{4}-\\d{4}", accountNumber);
+        if (!match){
+            throw new InvalidAccountNumberException();
+        }
     }
 
     public float getBalance() {
--- library/src/test/java/com/thoughtworks/AccountTest.java
+++ library/src/test/java/com/thoughtworks/AccountTest.java
@@ -8,23 +8,27 @@ import static org.junit.Assert.*;
 
 public class AccountTest {
 
-    private Account account;
+    private Account account1;
 
     @Before
-    public void setUp() throws MinimumExceptionError {
-        account = new Account("1234", 1000);
+    public void setUp() throws MinimumExceptionError, InvalidAccountNumberException {
+        account1 = new Account("1234-2345", 1000);
     }
 
     @Test
     public void shouldCheckBalance() {
         float balance  = 1000.0f;
-        assertThat(account.getBalance(),is(balance));
+        assertThat(account1.getBalance(),is(balance));
     }
-
-
 
     @Test(expected = MinimumExceptionError.class)
-    public void shouldCheckForMinimumBalance() throws MinimumExceptionError {
-        new Account("2345",200);
+    public void shouldCheckForMinimumBalance() throws MinimumExceptionError, InvalidAccountNumberException {
+        new Account("2345-3232",200);
     }
+
+    @Test(expected = InvalidAccountNumberException.class)
+    public void shouldValidateAccountNumber() throws MinimumExceptionError, InvalidAccountNumberException {
+       Account account =new Account("1234",1000);
+    }
+
 }
